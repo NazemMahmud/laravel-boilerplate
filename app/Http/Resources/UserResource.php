31935d6f62bef659c01148719c62264672b07845
Piash <nazem.piash10@gmail.com>
2022-03-12 23:59:38 +0600

    redis: include for user profile

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,7 +20,6 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ];
     }
 }
